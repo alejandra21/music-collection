8fea10b8e0ab5130760c441fbf838443c49edf7f
Alejandra Cordero <acorderog@socialh4ck.com>
2018-06-18 16:33:03 -0400

    - Se agregó el modelo de "People"

--- music-collection/src/main/java/com/test/musiccollection/model/People.java
+++ music-collection/src/main/java/com/test/musiccollection/model/People.java
@@ -5,10 +5,55 @@
  */
 package com.test.musiccollection.model;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  *
  * @author alejandra
  */
 public class People {
+
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name="name", unique=true)
+    private String name;
+
+    @Column(name="years", unique=true)
+    private Integer years;
+
+    public People(String name, Integer year){
+        this.name = name;
+        this.years = year;
+    }
+
+    // Getter
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public Integer getYears(){
+        return years;
+    }
+
+    // Setter
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setYears(Integer years){
+        this.years = years;
+    }
     
 }
