d33a05bea2119c5a1d14a0e9e45dfeba56605eae
Alejandra Cordero <acorderog@socialh4ck.com>
2018-06-18 21:48:15 -0400

    - Agregada relacion de PEOPLE y ARTISTS

--- music-collection/src/main/java/com/test/musiccollection/model/Artist.java
+++ music-collection/src/main/java/com/test/musiccollection/model/Artist.java
@@ -5,11 +5,15 @@
  */
 package com.test.musiccollection.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Index;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
@@ -22,6 +26,10 @@ public class Artist{
     @Id
     @GeneratedValue(strategy=GenerationType.SEQUENCE)
     private Long id;
+
+    // People associated with an artist.
+    @OneToMany(mappedBy = "merchant", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<People> members = new ArrayList<>();
 
     @Column(name="name", unique=true)
     private String name;
@@ -46,6 +54,10 @@ public class Artist{
         return years;
     }
 
+    public List<People> getMembers() {
+        return members;
+    }
+    
     // Setter
     public void setName(String name){
         this.name = name;
@@ -53,6 +65,16 @@ public class Artist{
 
     public void setYears(Integer years){
         this.years = years;
+    }
+
+    public void addMember(People member) {
+        members.add(member);
+        //member.setArtist(this);
+    }
+    
+    public void removeMember(People member) {
+        members.remove(member);
+        //member.setArtist(null);
     }
 
 }
