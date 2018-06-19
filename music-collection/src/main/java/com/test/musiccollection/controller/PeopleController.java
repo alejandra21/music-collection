6c6206be2d77303352e4f813a0bf65e6a8228d51
Alejandra Cordero <acorderog@socialh4ck.com>
2018-06-19 03:42:25 -0400

    -Se esta empezando a implementar los controladores de People

--- music-collection/src/main/java/com/test/musiccollection/controller/PeopleController.java
+++ music-collection/src/main/java/com/test/musiccollection/controller/PeopleController.java
@@ -5,10 +5,37 @@
  */
 package com.test.musiccollection.controller;
 
+import com.test.musiccollection.MessageResponse;
+import com.test.musiccollection.repository.PeopleRepository;
+import com.test.musiccollection.service.ServiceAddElements;
+import com.test.musiccollection.service.ServiceDeleteElements;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  *
  * @author alejandra
  */
+
+@Controller
 public class PeopleController {
+    
+    @Autowired
+    PeopleRepository peopleRepo;
+    @Autowired
+    private ServiceAddElements addElements;
+    @Autowired
+    private ServiceDeleteElements deleteElements;
+    public  MessageResponse messageResponse = new MessageResponse();
+    
+    @RequestMapping(value="/people", method=RequestMethod.GET)
+    public String allStyles(Model model) {
+        
+        model.addAttribute("elements", peopleRepo.findAll());
+        return "showElements";
+    }
     
 }
