a1a63331c1a3a2d8116fbec0b01d80b3a8e9c70a
Alejandra Cordero <acorderog@socialh4ck.com>
2018-06-19 00:05:34 -0400

    - Se agrega primer controller (listar styles)

--- music-collection/src/main/java/com/test/musiccollection/controller/StyleController.java
+++ music-collection/src/main/java/com/test/musiccollection/controller/StyleController.java
@@ -5,10 +5,33 @@
  */
 package com.test.musiccollection.controller;
 
+import com.test.musiccollection.model.Style;
+import com.test.musiccollection.repository.StyleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  *
  * @author alejandra
  */
+
+@Controller
 public class StyleController {
+    
+    @Autowired
+     StyleRepository styleRepo;
+    
+    
+    @RequestMapping(value="/style", method=RequestMethod.GET)
+    public String allStyles(Model model) {
+        
+        System.out.println("ESTE ES EL RESULTADO");
+        System.out.println(styleRepo.findAll());
+        model.addAttribute("elements", styleRepo.findAll());
+        return "showElements";
+    }
     
 }
